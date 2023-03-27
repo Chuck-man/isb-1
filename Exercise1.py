aeaf0d3fc52085239c3d7594fff99bb691926087
Chuck-man <sgold2398@gmail.com>
2023-03-27 18:21:46 +0400

    Replacing the alphabet of the text with a cipher

--- Exercise1.py
+++ Exercise1.py
@@ -1,33 +1,15 @@
-def code(file: str) -> None:
-    alfavit = "АБВГДЕЖЗИЙКЛМОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
-    dec_alfavit = "ГДЕЖЗИЙКЛМОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВ"
+def encryption(file: str):
+    alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
+    dec_alphabet = "гдежзийклмнопрстуфхцчшщъыьэюяабв"
     text = []
 
     with open(file, encoding="utf=8") as f:
-        for j in f:
-            for i in j:
-                i = i.upper()
-                place = alfavit.find(i)
-                if i in alfavit:
-                    text.append(dec_alfavit[place])
+        for i in f:
+            for j in i:
+                j = j.lower()
+                finding = alphabet.find(j)
+                if j in alphabet:
+                    text.append(dec_alphabet[finding])
                 else:
                     text.append(i)
-
-    new_file = open('output.txt', 'w')
-    for element in text:
-        new_file.write(element)
-    new_file.close()
-
-    new_file = open('alfavit.txt', 'w')
-    new_file.write("Ключ: ")
-    for element in dec_alfavit:
-        new_file.write(element)
-    new_file.write("\n")
-    new_file.write("      ")
-    for element in alfavit:
-        new_file.write(element)
-    new_file.close()
-
-
-if __name__ == "__main__":
-    code("test.txt")
+                    
