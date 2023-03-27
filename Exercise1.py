4184ba66467c96029dd60e113b68ae83df2e4493
Chuck-man <sgold2398@gmail.com>
2023-03-27 18:06:41 +0400

    Creating a function, and setting the alphabet

---
+++ Exercise1.py
@@ -0,0 +1,33 @@
+def code(file: str) -> None:
+    alfavit = "АБВГДЕЖЗИЙКЛМОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+    dec_alfavit = "ГДЕЖЗИЙКЛМОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВ"
+    text = []
+
+    with open(file, encoding="utf=8") as f:
+        for j in f:
+            for i in j:
+                i = i.upper()
+                place = alfavit.find(i)
+                if i in alfavit:
+                    text.append(dec_alfavit[place])
+                else:
+                    text.append(i)
+
+    new_file = open('output.txt', 'w')
+    for element in text:
+        new_file.write(element)
+    new_file.close()
+
+    new_file = open('alfavit.txt', 'w')
+    new_file.write("Ключ: ")
+    for element in dec_alfavit:
+        new_file.write(element)
+    new_file.write("\n")
+    new_file.write("      ")
+    for element in alfavit:
+        new_file.write(element)
+    new_file.close()
+
+
+if __name__ == "__main__":
+    code("test.txt")
